1a50e9e552ccc62ba013247e20c5f5a6c18c7c69
Elio Schmutz <e.schmutz@4teamwork.ch>
2018-05-25 19:36:34 +0200

    feat: implement receiving all userswq

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,10 +1,15 @@
 import { receiveQuestions } from './questions'
+import { receiveUsers } from './users'
 import { _getQuestions } from '../_DATA.js'
+import { _getUsers } from '../_DATA.js'
 
 export function handleInitialData() {
   return (dispatch) => {
     _getQuestions().then((questions) => {
       dispatch(receiveQuestions(questions))
+    })
+    _getUsers().then((users) => {
+      dispatch(receiveUsers(users))
     })
   }
 }
