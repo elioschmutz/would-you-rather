e34990ffe7ac2bacfcdd16130d7500219a5c43cc
Elio Schmutz <e.schmutz@4teamwork.ch>
2018-05-26 01:53:45 +0200

    feat: respect authedUser in main navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,50 +5,63 @@ import FaIconPlus from 'react-icons/lib/fa/plus'
 import FaListOl from 'react-icons/lib/fa/list-ol'
 import FaAngleLeft from 'react-icons/lib/fa/angle-left'
 import UserName from './UserName.js'
-
+import { connect } from 'react-redux'
 
 class Navigation extends Component {
   render() {
     return (
       <nav className="main-navigation navbar navbar-expand-md navbar-dark bg-primary">
-        <Link to="/" className="navbar-brand">Would you rather?</Link>
-        <div className="collapse navbar-collapse" id="main-navigation">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link to="/" className="nav-link"><FaIconHome /> Overview</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/add" className="nav-link"><FaIconPlus /> Add question</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/leaderboard" className="nav-link"><FaListOl /> Leaderboard</Link>
-            </li>
-          </ul>
-          <ul className="navbar-nav">
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link"
-                href="#"
-                id="navbar-user-dropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                <UserName />
-              </a>
-              <div
-                className="dropdown-menu dropdown-menu-right"
-                aria-labelledby="navbar-user-dropdown"
-              >
-                <Link to="/logout" className="dropdown-item"><FaAngleLeft /> Logout</Link>
-              </div>
-            </li>
-          </ul>
-        </div>
+        <Link to="/" className="navbar-brand">
+          Would you rather?
+        </Link>
+        {this.props.authedUser && (
+          <div className="collapse navbar-collapse" id="main-navigation">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  <FaIconHome /> Overview
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/add" className="nav-link">
+                  <FaIconPlus /> Add question
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/leaderboard" className="nav-link">
+                  <FaListOl /> Leaderboard
+                </Link>
+              </li>
+            </ul>
+            <ul className="navbar-nav">
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link"
+                  href="#"
+                  id="navbar-user-dropdown"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  <UserName />
+                </a>
+                <div
+                  className="dropdown-menu dropdown-menu-right"
+                  aria-labelledby="navbar-user-dropdown"
+                >
+                  <Link to="/logout" className="dropdown-item">
+                    <FaAngleLeft /> Logout
+                  </Link>
+                </div>
+              </li>
+            </ul>
+          </div>
+        )}
       </nav>
     )
   }
 }
 
-export default Navigation
+const mapStateToProps = ({ authedUser }) => ({ authedUser })
+export default connect(mapStateToProps)(Navigation)
